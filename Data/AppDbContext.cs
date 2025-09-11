e0a4bc739468953e441c8462fe2f7b2b8080d737
Victoria Frias Muñoz <a2024112803@estudiantes.upsa.edu.bo>
2025-09-10 21:30:32 -0400

    Create MoodEntries Migrations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using WellnesHubAPI.Data;
 using WellnesHubAPI.models;
+using WellnesHubAPI.Models;
 
 namespace WellnesHubAPI.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Habit> Habits { get; set; }
-    
+    public DbSet<MoodEntry> MoodEntries { get; set; }
 }
---
+++ Migrations/20250911012924_AddMoodEntriesTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WellnesHubAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoodEntriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MoodEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EntryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Mood = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Energy = table.Column<int>(type: "int", nullable: true),
+                    SleepHours = table.Column<double>(type: "float", nullable: true),
+                    StressLevel = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoodEntries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoodEntries");
+        }
+    }
+}
